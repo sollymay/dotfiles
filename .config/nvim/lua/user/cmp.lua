4daccdde1d5e0ddaa4867bffa32360cbe4065a32
Salomon May <salomon_may@apple.com>
2022-08-16 10:47:34 -0400

    fixed some stuff and added yabai on mac

--- .config/nvim/lua/user/cmp.lua
+++ .config/nvim/lua/user/cmp.lua
@@ -5,7 +5,7 @@ require("luasnip.loaders.from_vscode").load()
 local cmp = require "cmp"
 
 local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 -- LSP Symbols
@@ -56,12 +56,11 @@ cmp.setup{
   },
  mapping = {
     ["<s-tab>"] = cmp.mapping.select_prev_item(),
-    ["<tab>"] = cmp.mapping.select_next_item(),
   -- ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
   -- ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
   -- ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ["<Tab>"] = cmp.mapping(function()
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
@@ -69,7 +68,7 @@ cmp.setup{
       elseif has_words_before() then
         cmp.complete()
       else
-        fallback()
+        cmp.mapping.select_next_item()
       end
     end, { "i", "s" }),
     ["<S-Tab>"] = cmp.mapping(function(fallback)
