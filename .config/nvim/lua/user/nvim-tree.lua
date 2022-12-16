4eeaba212cbcf88c9fd414705a266ccea737e6b3
Salomon May <sollymay@gmail.com>
2022-12-16 10:47:19 -0500

    fixed nvim-tree overall look and feel

--- .config/nvim/lua/user/nvim-tree.lua
+++ .config/nvim/lua/user/nvim-tree.lua
@@ -1,32 +1,47 @@
 -- NerdTree alternative
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
-  return
+    return
 end
 
 local tree_cb = require 'nvim-tree.config'.nvim_tree_callback
 vim.g.view_mappings_list = {
-     ["<C-.>"] = tree_cb("edit_in_place"),
+    ["<C-.>"] = tree_cb("edit_in_place"),
 }
 
 nvim_tree.setup {
 
-open_on_setup = true,
-view = {
-    width = 50,
-    number = true,
-    relativenumber = true,
-},
-git = {
-    ignore = false,
-},
-actions = {
+    open_on_setup = true,
+    view = {
+        width = 50,
+        number = true,
+        relativenumber = false,
+    },
+    git = {
+        ignore = false,
+    },
+    actions = {
         open_file = {
             quit_on_open = true
         },
         change_dir = {
             enable = false,
         },
-    }
-}
+    },
+    renderer = {
+        indent_width = 2,
+        indent_markers = {
+            enable = true,
+            inline_arrows = false,
+            icons = {
+                bottom = ""
+            }
+        },
+        icons = {
+            show = {
+                folder_arrow = false,
+            }
+        }
+    },
 
+}
