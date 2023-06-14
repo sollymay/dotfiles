0a1800621994a7b1eec537b2f24f60cd19e1b355
Salomon May <salomon_may@apple.com>
2023-06-14 14:14:35 -0400

    added p10k config

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,20 +1,19 @@
-
-return {'nvim-lualine/lualine.nvim',
-
-config = function()
-require("lualine").setup {
-    options = {
-        theme = 'dracula',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-    },
-    sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { '%=', 'filename' },
-        lualine_x = { 'filetype' },
-        lualine_z = { 'location' },
-    },
-}
-end
+return {
+    'nvim-lualine/lualine.nvim',
+    config = function()
+        require("lualine").setup {
+            options = {
+                theme = 'dracula',
+                component_separators = '',
+                section_separators = { left = '', right = '' },
+            },
+            sections = {
+                lualine_a = { { 'mode', separator = { left = '', right = '' }, right_padding = 2 } },
+                lualine_b = { 'branch', 'diff', 'diagnostics' },
+                lualine_c = { '%=', 'filename' },
+                lualine_x = { 'filetype' },
+                lualine_z = { { 'location', separator = { left = '', right = '' }, right_padding = 2 } },
+            },
+        }
+    end
 }
