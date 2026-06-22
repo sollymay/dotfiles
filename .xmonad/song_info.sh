659ba85b2f9abef3cdfb1d4ba1b3d31423bc92ee
Salomon May <sollymay@users.noreply.github.com>
2026-06-22 13:15:25 -0400

    Delete .xmonad directory

--- .xmonad/song_info.sh
+++
@@ -1,8 +0,0 @@
-running=$(playerctl status)
-if [ "$running" == "Playing" ]; then
-    artist=$(playerctl metadata artist)
-    song=$(playerctl metadata title | cut -c 1-60)
-    echo -n " $artist - $song "
-else
-    echo -n " No media playing"
-fi
